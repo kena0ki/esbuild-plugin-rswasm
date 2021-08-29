f14580e56605fd8b51e6e61ddac29651fe0e247a
kena0ki <ken3574.25@gmail.com>
2021-08-29 17:44:40 +0900

    ffi

--- rs/lib.rs
+++ rs/lib.rs
@@ -5,3 +5,9 @@ mod tests {
         assert_eq!(2 + 2, 4);
     }
 }
+
+#[no_mangle]
+pub extern fn double_input(input: i32) -> i32 {
+    input * 2
+}
+
--- ts/index.ts
+++ ts/index.ts
@@ -1,1 +1,11 @@
 import type { PluginBuild, PartialMessage, Loader } from 'esbuild';
+import ffi from 'ffi-napi';
+
+const lib = ffi.Library('target/debug/libwasmpack_wrapper', {
+  'double_input': [ 'int', [ 'int' ] ]
+});
+
+const input = 4;
+const output = lib.double_input(input);
+console.log(input + " * 2 = " + output);
+
